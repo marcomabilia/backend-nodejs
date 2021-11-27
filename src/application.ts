3bed45fc508015fd56aef7edde948255d7c78be3
Marco Martins <marcoamm@gmail.com>
2021-11-27 14:06:37 -0300

    Entregue para avaliação.

--- src/application.ts
+++ src/application.ts
@@ -6,14 +6,28 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const application = express()
+const path = require('path');
 
-application.use(bodyParser.text())
-application.use(express.json())
-application.use(express.urlencoded({ extended: false }))
-application.use(cors())
+application.engine('html', require('ejs').renderFile);
+application.set('view engine','html');
+application.use('/public',express.static(path.join(__dirname,'public')));
+application.set('views', path.join(__dirname,'views'));
 
-application.get('/', (req, res) => {
-    res.send('Hello World!')
+
+application.get('/saque/cc/:quantia', (req, res) => {
+   res.render('index',{quantia:req.params.quantia , saldo:1500, tipoConta:'Conta Corrente', tipo:2})
+})
+
+application.get('/saque/poup/:quantia', (req, res) => {
+    res.render('index',{quantia:req.params.quantia, saldo:400 , tipoConta:'Poupança', tipo:2})
+})
+
+application.get('/deposito/cc/:quantia', (req, res) => {
+    res.render('index',{quantia: req.params.quantia , saldo:3000 , tipoConta:'Conta Corrente' , tipo:1, })
+})
+
+application.get('/deposito/poup/:quantia', (req, res) => {
+    res.render('index',{quantia: req.params.quantia , saldo:500 , tipoConta:'Poupança' , tipo:1, })
 })
 
 application.set('port', process.env.APP_PORT || 5000)
